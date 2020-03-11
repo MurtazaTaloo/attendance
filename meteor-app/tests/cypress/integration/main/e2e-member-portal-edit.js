558b4bc6e92a62e41fef9070eaac7a7ece39d22e
Patrick Carmel <pato@live.com.au>
2020-03-11 14:22:02 +1100

    added a member-edit-portal to test if buttons  will work as required

---
+++ meteor-app/tests/cypress/integration/main/e2e-member-portal-edit.js
@@ -0,0 +1,14 @@
+/* Pseudo code for testing member portal buttons
+  * Navigate to /login
+  * enter  login details
+  * click on the member portal page
+  * enter members details
+
+  * open up test members portal
+  * click on the edit profile button
+  * Determine whether it retrieves the edit profile webpage
+  * 
+  * find a test member
+  * Enter a WWCC which is expired
+  * Check for error message
+*/
